43deeb428f233f832aba27c32f2bee8a98f9892c
RenanBrigagao <renan.brigagao@gmailcom>
2026-06-22 22:08:10 -0300

    Level 2 implementation

--- Code/const.py
+++ Code/const.py
@@ -5,7 +5,8 @@ C_WHITE = (255, 255, 255)
 C_PINK = (179, 93, 255)
 
 # E
-EVENT_ENEMY = pygame.USEREVENT +1
+EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 
 ENTITY_SPEED = {
     'Level1Bg0': 0,
@@ -17,6 +18,14 @@ ENTITY_SPEED = {
     'Level1Bg6': 6,
     'Level1Bg7': 7,
     'Level1Bg8': 8,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
+    'Level2Bg5': 5,
+    'Level2Bg6': 6,
+    'Level2Bg7': 7,
     'Player1': 4,
     'Player1Shot': 2,
     'Player2': 4,
@@ -41,6 +50,9 @@ ENTITY_HEALTH = {
     'Level2Bg2': 999,
     'Level2Bg3': 999,
     'Level2Bg4': 999,
+    'Level2Bg5': 999,
+    'Level2Bg6': 999,
+    'Level2Bg7': 999,
     'Player1': 300,
     'Player1Shot': 1,
     'Player2': 300,
@@ -71,6 +83,9 @@ ENTITY_DAMAGE = {
     'Level2Bg2': 0,
     'Level2Bg3': 0,
     'Level2Bg4': 0,
+    'Level2Bg5': 0,
+    'Level2Bg6': 0,
+    'Level2Bg7': 0,
     'Player1': 1,
     'Player1Shot': 25,
     'Player2': 1,
@@ -96,6 +111,9 @@ ENTITY_SCORE = {
     'Level2Bg2': 0,
     'Level2Bg3': 0,
     'Level2Bg4': 0,
+    'Level2Bg5': 0,
+    'Level2Bg6': 0,
+    'Level2Bg7': 0,
     'Player1': 0,
     'Player1Shot': 0,
     'Player2': 0,
@@ -125,6 +143,13 @@ PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
                     'Player2': pygame.K_d}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_p,
                     'Player2': pygame.K_g}
+
+# S
+SPAWN_TIME = 2000
+
+# T
+TIMEOUT_STEP = 100
+TIMEOUT_LEVEL = 30000
 
 # W
 WIN_WIDTH = 576
--- Code/level.py
+++ Code/level.py
@@ -8,7 +8,8 @@ from pygame._freetype import Font
 from pygame.rect import Rect
 from pygame.surface import Surface
 
-from Code.const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, C_PINK
+from Code.const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, C_PINK, SPAWN_TIME, EVENT_TIMEOUT, TIMEOUT_STEP, \
+    TIMEOUT_LEVEL
 from Code.enemy import Enemy
 from Code.entity import Entity
 from Code.entityFactory import EntityFactory
@@ -17,19 +18,24 @@ from Code.player import Player
 
 
 class Level:
-    def __init__(self, screen, name, game_mode):
-        self.timeout = 20000
+    def __init__(self, screen, name, game_mode, player_score: list[int]):
+        self.timeout = TIMEOUT_LEVEL
         self.screen = screen
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
-        self.entity_list.extend(EntityFactory.get_entity('Level1Bg', (0, 0)))
-        self.entity_list.append(EntityFactory.get_entity('Player1', (0, 0)))
+        self.entity_list.extend(EntityFactory.get_entity(self.name + 'Bg', (0, 0)))
+        player = EntityFactory.get_entity('Player1', (0, 0))
+        player.score = player_score[0]
+        self.entity_list.append(player)
         if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
-            self.entity_list.append(EntityFactory.get_entity('Player2', (0, 0)))
-        pygame.time.set_timer(EVENT_ENEMY, 2000)
+            player = EntityFactory.get_entity('Player2', (0, 0))
+            player.score = player_score[1]
+            self.entity_list.append(player)
+        pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
+        pygame.time.set_timer(EVENT_TIMEOUT, TIMEOUT_STEP)
 
-    def run(self):
+    def run(self, player_score: list[int]):
         pygame.mixer_music.load(f'./asset/{self.name}.wav')
         pygame.mixer_music.play(-1)
         clock = pygame.time.Clock()
@@ -52,6 +58,24 @@ class Level:
                 if event.type == EVENT_ENEMY:
                     choice = random.choice(('Enemy1', 'Enemy2'))
                     self.entity_list.append(EntityFactory.get_entity(choice))
+                if event.type == EVENT_TIMEOUT:
+                    self.timeout -= TIMEOUT_STEP
+                    if self.timeout == 0:
+                        for ent in self.entity_list:
+                            if isinstance(ent, Player) and ent.name =='Player1':
+                                player_score[0] = ent.score
+                        for ent in self.entity_list:
+                            if isinstance(ent, Player) and ent.name =='Player2':
+                                player_score[1] = ent.score
+                        return True
+
+                found_player = False
+                for ent in self.entity_list:
+                    if isinstance(ent, Player):
+                        found_player = True
+
+                if not found_player:
+                    return False
 
             self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', C_WHITE, (10, 5))
             self.level_text(14, f'fps: {clock.get_fps():.0f}', C_WHITE, (10, WIN_HEIGHT - 35))
@@ -61,8 +85,6 @@ class Level:
 
             EntityMediator.verify_collision(entity_list=self.entity_list)
             EntityMediator.verify_health(entity_list=self.entity_list)
-
-            pass
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
